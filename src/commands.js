4188a6be938aa5d0affa89b92f1d1636b1322b46
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-05-02 10:24:55 +0200

    Fix lint in commands.js

--- src/commands.js
+++ src/commands.js
@@ -1,6 +1,6 @@
 import {
-	ApplicationCommandType,
-	ApplicationCommandOptionType
+  ApplicationCommandType,
+  ApplicationCommandOptionType
 } from 'discord-interactions';
 
 /**
@@ -12,71 +12,71 @@ export const TIMESTAMP_COMMAND = {
   name: 'timestamp',
   description: 'Generate Discord timestamps based on a given time and timezone',
   type: ApplicationCommandType.ChatInput,
-	options: [
-		{
-			name: 'day',
-			description: 'Day (1 - 31)',
-			required: true,
-			type: ApplicationCommandOptionType.Integer
-		},
-		{
-			name: 'month',
-			description: 'Month (1 - 12)',
-			required: true,
-			type: ApplicationCommandOptionType.Integer
-		},
-		{
-			name: 'year',
-			description: 'Year (min. 1970)',
-			required: true,
-			type: ApplicationCommandOptionType.Integer
-		},
-		{
-			name: 'hour',
-			description: 'Hour (0 - 23)',
-			required: true,
-			type: ApplicationCommandOptionType.Integer
-		},
-		{
-			name: 'minute',
-			description: 'Minute (0 - 59)',
-			required: true,
-			type: ApplicationCommandOptionType.Integer
-		},
-		{
-			name: 'timezone',
-			description: 'Timezone (Format: continent/city)',
-			required: true,
-			type: ApplicationCommandOptionType.String,
-			autocomplete: true
-		},
-		{
-			name: 'second',
-			description: 'Second (0 - 59)',
-			required: false,
-			type: ApplicationCommandOptionType.Integer
-		},
-		{
-			name: 'public',
-			description: 'Should the response be visible to everyone? (Default: False)',
-			required: false,
-			type: ApplicationCommandOptionType.Boolean
-		}
-	]
+  options: [
+    {
+      name: 'day',
+      description: 'Day (1 - 31)',
+      required: true,
+      type: ApplicationCommandOptionType.Integer
+    },
+    {
+      name: 'month',
+      description: 'Month (1 - 12)',
+      required: true,
+      type: ApplicationCommandOptionType.Integer
+    },
+    {
+      name: 'year',
+      description: 'Year (min. 1970)',
+      required: true,
+      type: ApplicationCommandOptionType.Integer
+    },
+    {
+      name: 'hour',
+      description: 'Hour (0 - 23)',
+      required: true,
+      type: ApplicationCommandOptionType.Integer
+    },
+    {
+      name: 'minute',
+      description: 'Minute (0 - 59)',
+      required: true,
+      type: ApplicationCommandOptionType.Integer
+    },
+    {
+      name: 'timezone',
+      description: 'Timezone (Format: continent/city)',
+      required: true,
+      type: ApplicationCommandOptionType.String,
+      autocomplete: true
+    },
+    {
+      name: 'second',
+      description: 'Second (0 - 59)',
+      required: false,
+      type: ApplicationCommandOptionType.Integer
+    },
+    {
+      name: 'public',
+      description: 'Should the response be visible to everyone? (Default: False)',
+      required: false,
+      type: ApplicationCommandOptionType.Boolean
+    }
+  ]
 };
 
 export const CURRENT_TIMESTAMP_COMMAND = {
   name: 'currenttimestamp',
   description: 'Generate Discord timestamps based on the current time',
   type: ApplicationCommandType.ChatInput,
-	options: [
-		{
-			name: 'public',
-			description: 'Should the response be visible to everyone? (Default: False)',
-			required: false,
-			type: ApplicationCommandOptionType.Boolean
-		}
-	]
+  options: [
+    {
+      name: 'public',
+      description: 'Should the response be visible to everyone? (Default: False)',
+      required: false,
+      type: ApplicationCommandOptionType.Boolean
+    }
+  ]
 };
 
 export const INVITE_COMMAND = {
